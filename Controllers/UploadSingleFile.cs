f7e45e681384e5c1edf415bd96a5ce3a74601382
Siqi Liu <siqiliu18@gmail.com>
2020-04-24 23:21:46 -0700

    store input file into created Data dir

--- Controllers/UploadSingleFile.cs
+++ Controllers/UploadSingleFile.cs
@@ -17,7 +17,10 @@ namespace HelloAspNetCore.Controllers
         {
 
             // Full path to file in temp location
-            var filePath = Path.GetTempFileName();
+            //var filePath = Path.GetTempFileName();
+            var dataDir = Directory.GetCurrentDirectory() + "/Data/";
+            Directory.CreateDirectory(dataDir);
+            var filePath = dataDir + file.FileName;
 
             if (file.Length > 0)
                 using (var stream = new FileStream(filePath, FileMode.Create))
