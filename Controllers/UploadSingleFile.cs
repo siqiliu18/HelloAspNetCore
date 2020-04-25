042857ac8bdbb49cf52f0068184f9c6d2ca63523
Siqi Liu <siqiliu18@gmail.com>
2020-04-24 18:16:07 -0700

    an POST endpoint for uploading file

---
+++ Controllers/UploadSingleFile.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloAspNetCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadSingleFileController : ControllerBase
+    {
+        [HttpPost("UploadSingleFile")]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+
+            // Full path to file in temp location
+            var filePath = Path.GetTempFileName();
+
+            if (file.Length > 0)
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                    await file.CopyToAsync(stream);
+
+            // Process uploaded files
+
+            return Ok(new { count = 1, path = filePath});
+        }
+    }
+}
